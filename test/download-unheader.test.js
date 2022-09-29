c68c7f771c075b436e7d64cca62445038093e851
阮家友 <1439120442@qq.com>
2022-09-29 16:07:09 +0800

    github 有墙啊

--- test/download-unheader.test.js
+++ test/download-unheader.test.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const url = 'https://github.com/TencentCloud/tencentcloud-sdk-nodejs/archive/refs/heads/master.zip';
+const url = 'https://upload-images.jianshu.io/upload_images/2675631-4bef7ddf67d042e9.gif?imageMogr2/auto-orient/strip|imageView2/2/w/774/format/webp';
 (async () => {
   await shttp.download(url, __dirname + '/files/' + path.basename(url)).progress((count, total) => {
     console.log(`${count}/${total}`)
